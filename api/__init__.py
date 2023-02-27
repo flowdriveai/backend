b66ab8d7305e69880a130d9a0bce64d4c9f46b70
Gunwant Jain <mail@wantguns.dev>
2023-02-27 05:35:05 +0530

    flask: include a constraints naming template to avoid migration errors
    Signed-off-by: Gunwant Jain <mail@wantguns.dev>

--- api/__init__.py
+++ api/__init__.py
@@ -1,5 +1,6 @@
 import os
 
+from sqlalchemy import MetaData
 from flask import Flask
 from flask_bcrypt import Bcrypt
 from flask_sqlalchemy import SQLAlchemy
@@ -16,9 +17,18 @@ app_settings = os.getenv(
 )
 app.config.from_object(app_settings)
 
+convention = {
+    "ix": 'ix_%(column_0_label)s',
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(constraint_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s"
+}
+
+metadata = MetaData(naming_convention=convention)
 bcrypt = Bcrypt(app)
-db = SQLAlchemy(app)
-migrate = Migrate(app, db) 
+db = SQLAlchemy(app, metadata=metadata)
+migrate = Migrate(app, db, render_as_batch=True)
 mail = Mail(app)
 
 from api.views.urls import auth_bp, user_bp, drive_bp
